ffb589543f67c0365915e956b80cb91d1de81de3
OreoHolicookie <154807378+OreoHolicookie@users.noreply.github.com>
2025-03-28 10:03:08 +0800

    Update initialize.php

--- initialize.php
+++ initialize.php
@@ -5,4 +5,4 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
 if(!defined('DB_NAME')) define('DB_NAME',"wine");
-?>ss
+?>
